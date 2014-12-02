f9fc95f99a4746e5591bf2b23f32fb0a5498755e
ThePenultimateOne <gappleto97@gmail.com>
2014-12-02 10:24:22 -0500

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -21,22 +21,18 @@ $LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
 $REMOTE_REPO        = "https://github.com/ColdLogic-3617/Webiste.git";
 $BRANCH             = "master";
 
-if ( $_POST['payload'] ) {
-  // Only respond to POST requests from Github
+if( file_exists($LOCAL_REPO) ) {
 
-  if( file_exists($LOCAL_REPO) ) {
+  // If there is already a repo, just run a git pull to grab the latest changes
+  shell_exec("cd {$LOCAL_REPO} && git pull");
 
-    // If there is already a repo, just run a git pull to grab the latest changes
-    shell_exec("cd {$LOCAL_REPO} && git pull");
+  die("done " . mktime());
+} else {
 
-    die("done " . mktime());
-  } else {
+  // If the repo does not exist, then clone it into the parent directory
+  shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO}");
 
-    // If the repo does not exist, then clone it into the parent directory
-    shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO}");
-
-    die("done " . mktime());
-  }
+  die("done " . mktime());
 }
 
 ?>
