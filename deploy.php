76ee24670d4fc479946c3d81ef511c66d26cd723
ThePenultimateOne <gappleto97@gmail.com>
2014-12-05 10:40:31 -0500

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,19 +1,4 @@
 <?php
-/** 
-  * This script is for easily deploying updates to Github repos to your local server. It will automatically git clone or 
-  * git pull in your repo directory every time an update is pushed to your $BRANCH (configured below).
-  * 
-  * INSTRUCTIONS:
-  * 1. Edit the variables below
-  * 2. Upload this script to your server somewhere it can be publicly accessed
-  * 3. Make sure the apache user owns this script (e.g., sudo chown www-data:www-data webhook.php)
-  * 4. (optional) If the repo already exists on the server, make sure the same apache user from step 3 also owns that 
-  *    directory (i.e., sudo chown -R www-data:www-data)
-  * 5. Go into your Github Repo > Settings > Service Hooks > WebHook URLs and add the public URL 
-  *    (e.g., http://example.com/webhook.php)
-  *
-**/
-
 // Set Variables
 $LOCAL_ROOT         = "/home/gappleto";
 $LOCAL_REPO_NAME    = "pub";
@@ -21,11 +6,9 @@ $LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
 $REMOTE_REPO        = "https://github.com/ColdLogic-3617/Webiste.git";
 $BRANCH             = "master";
 
+$output shell_exec("cd {$LOCAL_REPO} && git pull");
 
-
-// If there is already a repo, just run a git pull to grab the latest changes
-shell_exec("cd {$LOCAL_REPO} && git pull");
-
-die("done " . mktime());
+die("done " . mktime()
+$output);
 
 ?>
