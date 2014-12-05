0ea58b30189e39803fbc7dca6eb2d8501b7e2160
ThePenultimateOne <gappleto97@gmail.com>
2014-12-05 10:49:46 -0500

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -8,6 +8,6 @@ $BRANCH             = "master";
 
 $output = shell_exec("cd {$LOCAL_REPO} && git pull");
 
-die("done " . date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']) . "<br><pre>$output</pre>");
+die(date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']) . "<br><pre>$output</pre>");
 
 ?>
